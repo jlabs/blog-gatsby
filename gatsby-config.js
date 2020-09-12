1f64f848c5464db143674c1f832390748a048f4f
jlabs <adams.j.6385@gmail.com>
2020-09-12 23:09:35 +0100

    Boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,24 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-transformer-sharp`, `gatsby-plugin-sharp`]
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
